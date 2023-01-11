0e5f6d3d2bf4aa2d60f30215aa63a0854e59c4e6
Luis Gregorio <gregoriobom@hotmail.com>
2023-01-11 17:56:25 -0300

    Initial commit

---
+++ src/test/java/com/crud/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
